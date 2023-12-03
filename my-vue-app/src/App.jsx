b50e45500a30f8ceba2f0c33cb9801e3a5400a9a
EmilKhamatnurov <107917011+EmilKhamatnurov@users.noreply.github.com>
2023-12-03 20:45:06 +0500

    Добавлен каунтер

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  // localStorage.setItem('counter', 0)
+  const [counter, setCounter] = useState(() => 
+    !parseInt(localStorage.getItem('counter')) ? 0 : 
+    parseInt(localStorage.getItem('counter'))
+  );
+
+  useEffect(() => {
+    localStorage.setItem('counter', counter)
+  }, [counter]);
 
   return (
-    <>Привет</>
+    <div>
+      {counter} <br />
+      <button onClick={() => setCounter(counter + 1)}>Добавить 1</button>
+    </div>
   )
 }
 
